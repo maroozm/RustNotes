a06dcc9e2b187d6e6d6a0cdd7cb1ad2f2e703560
Salman KMalik <salman.khurshid.malik@cern.ch>
2023-03-28 00:07:41 +0530

    commit

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,17 +1,14 @@
-fn first_word(s: &String) -> usize {
+fn first_word(s: &String) -> &str {
     let bytes = s.as_bytes();
 
     for(i, &item) in bytes.iter().enumerate() {
         if item == b' ' {
-            return i;
+            return &s[0..i];
         }
     }
-    s.len()
+    &s[..]
 }
 fn main() {
-    let mut s = String::from("hello world");
-
-    let word = first_word(&s);
-    s.clear();
+    let my_string = String::from("hello world");
+    
 }
-
